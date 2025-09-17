c7d7748760c6c9d6dfe8eb7c8149cc7f738c7b87
userdok <04kkukkum@gmail.com>
2025-09-17 20:12:05 +0900

    Initial Commit

---
+++ AlarmClockUITests/AlarmClockUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  AlarmClockUITestsLaunchTests.swift
+//  AlarmClockUITests
+//
+//  Created by 공학관559 on 9/17/25.
+//
+
+import XCTest
+
+final class AlarmClockUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
